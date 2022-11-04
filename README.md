fbd9b7b83a80f68e7d83dc3c01ad40dbdd33dd0c
Kevin Batchelor <you@example.com>
2022-11-04 16:34:48 -0700

    Maven, env, gitignore files for base project.

---
+++ README.md
@@ -0,0 +1,2 @@
+# DALL-E-OpenAI
+See the power of OpenAI's DALL-E AI generated images! Super cool!
---
+++ src/main/java/com/kevinbatchelor/openAIproject/OpenAIprojectApplication.java
@@ -0,0 +1,12 @@
+package com.kevinbatchelor.openAIproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAIprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAIprojectApplication.class, args);}
+
+}
